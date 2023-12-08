edf011051458eb5e297618d958e918fd36d23f5d
Mikha'el Wade <mwade729@gmail.com>
2023-12-08 17:23:32 +0000

    Test file upload.

--- src/main/java/org/mwade/assessment/controllers/EntryController.java
+++ src/main/java/org/mwade/assessment/controllers/EntryController.java
@@ -1,7 +1,12 @@
 package org.mwade.assessment.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class EntryController {
@@ -9,5 +14,12 @@ public class EntryController {
     @GetMapping("/hello")
     public String hello() {
         return "hi";
+    }
+
+    @PostMapping("/process")
+    public String processFile(@RequestParam("file") MultipartFile entryFile) throws IOException {
+        String entry = new String(entryFile.getBytes());
+        System.out.println(entry);
+        return entry;
     }
 }
